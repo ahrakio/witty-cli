9a2d6b04468cafce66915333c4496fd6f5ca678b
Uriah Ahrak <uriah.achrak@gmail.com>
2018-08-26 21:31:14 +0300

    Added serve command

---
+++ src/Commands/ServeCommand.ts
@@ -0,0 +1,108 @@
+import { Command } from 'commander';
+import { CommandAbstract } from './CommandAbstract';
+import * as path from 'path';
+import webpack from 'webpack';
+const spawn = require('child_process').spawn;
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+// Clean configurations
+const clean_paths = [
+    path.resolve(__dirname, '../../../witty-project/dist')
+];
+
+const clean_options = {
+    watch: true
+};
+
+export class ServeCommand extends CommandAbstract {
+    constructor() {
+        super();
+        
+        this.name = 'serve';
+        this.alias = 's';
+        this.params =  [];
+        this.description = 'Serve the project for dev-server';
+        this.options = [
+            { 
+                name: 'port',
+                char: 'p',
+                params: ['port'],
+                description: 'Set the port of the dev-server. Default is 8400'
+            }
+        ];  
+    }
+
+    protected handle(commands: Command[]): void {
+        let child: any = null;
+        let cwd = process.cwd();
+        let entryPath = path.resolve(cwd, '../witty-project/index.ts');
+        let outputPath = path.resolve(cwd, 'serve');
+
+        let config = {
+            entry: entryPath,
+            module: {
+                rules: [
+                    {
+                        test: /\.ts$/,
+                        use: 'ts-loader',
+                        exclude: /node_modules/
+                    }
+                ]
+            },
+            resolve: {
+                extensions: ['.ts', '.js']
+            },
+            output: {
+                filename: 'index.js',
+                path: outputPath,
+            },
+            target: 'node',
+            mode: 'development',
+            watchOptions: {
+                aggregateTimeout: 1000,
+                poll: 1000
+            },
+            plugins: [
+                new CleanWebpackPlugin(clean_paths, clean_options),
+                new UglifyJsPlugin(),
+                new webpack.ProgressPlugin((percentage, msg) => {
+                    if (percentage === 0) {
+                        if (child !== null) {
+                            child.kill('SIGKILL');
+                            child = null;
+                        }
+                    } else if (percentage === 1) {
+                        let args: string[] = [
+                            outputPath + '/index.js'
+                        ];
+        
+                        if (commands[0].port !== undefined) {
+                            args.push(commands[0].port);
+                        }
+        
+                        child = spawn('node', args);
+                    }
+                })
+            ]
+        };
+
+        const compiler = webpack(config);
+        compiler.watch({
+            poll: 300
+        }, (a, stats) => {
+            const info = stats.toJson();
+            if (stats.hasErrors()) {
+                console.error(info.errors);
+            }
+
+            if (stats.hasWarnings()) {
+                console.warn(info.warnings);
+            }
+
+            console.log(stats.toString({
+                colors: true
+            }));
+        });
+    }
+}
--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@ import commander from 'commander';
 import {GenerateCommand} from  "./Commands/GenerateCommand"
 import { BuildCommand } from './Commands/BuildCommand';
 import { CommandAbstract } from './Commands/CommandAbstract';
+import { ServeCommand } from './Commands/ServeCommand';
 
 let command: ICommand = new GenerateCommand();
 interface ExtendedOptions extends commander.CommandOptions {
@@ -31,6 +32,17 @@ commander
     .option('-' + command2.Options[i].char +', --' + command2.Options[i].name+' <' +
                 command2.Options[i].params[0]+'>', command2.Options[i].description)
     .action(command2.handler());
+
+
+let command3: CommandAbstract = new ServeCommand();
+commander
+    .version('0.1.0')
+    .command(command3.Name)
+    .alias(command3.Alias)
+    .description(command3.Description)
+    .option('-' + command3.Options[i].char +', --' + command3.Options[i].name+' <' +
+                command3.Options[i].params[0]+'>', command3.Options[i].description)
+    .action(command3.handler());
 // main parse
 commander.parse(process.argv);
 
