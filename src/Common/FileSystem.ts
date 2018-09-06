46394acf7b9ce5951e5ac595b7634a4288e7be36
Uriah Ahrak <uriah.achrak@gmail.com>
2018-09-06 15:47:15 +0300

    Some serve command changes

--- src/Common/FileSystem.ts
+++ src/Common/FileSystem.ts
@@ -1,47 +1,47 @@
-import {existsSync, mkdirSync, readFileSync, realpathSync, writeFileSync} from "fs";
-import {sep} from 'path'
+import { existsSync, mkdirSync, readFileSync, realpathSync, writeFileSync } from "fs";
+import { sep } from "path";
 
-export function touchDir(path:string) :void {
+export function touchDir(path: string): void {
     if (existsSync(path)) return;
-    let dirNames:string []  = path.split(sep);
+    let dirNames: string[] = path.split(sep);
     let soFar = process.cwd();
-    for (let i=0; i< dirNames.length; ++i) {
-        let dirName : string = dirNames[i];
+    for (let i = 0; i < dirNames.length; ++i) {
+        let dirName: string = dirNames[i];
         soFar = soFar + sep + dirName;
         if (!existsSync(soFar)) {
             mkdirSync(soFar);
-            console.log(dirName+ ' is created!');
+            console.log(dirName + " is created!");
         }
     }
 }
 
-export function readJsonFile (path:string) : object {
+export function readJsonFile(path: string): object {
     try {
-           return JSON.parse(readFileSync(path, "ascii"));
-       } catch {
+        return JSON.parse(readFileSync(path, "ascii"));
+    } catch {
         return null;
     }
 }
 
-export  function writeJsonFile (obj: object, path:string=process.cwd()) : boolean {
+export function writeJsonFile(obj: object, path: string = process.cwd()): boolean {
     try {
-        writeFileSync(path, JSON.stringify(obj,null, "\t"), 'ascii');
+        writeFileSync(path, JSON.stringify(obj, null, "\t"), "ascii");
         return true;
-    }catch  {
+    } catch {
         return false;
     }
 }
 
 /**
  * will return that path of file or null if not found. (search only recursive up)
-  */
-export function findFile (name:string) :string | null {
+ */
+export function findFile(name: string): string | null {
     let candidate = process.cwd();
-    while (candidate.indexOf(sep)!== -1) {
+    while (candidate.indexOf(sep) !== -1) {
         if (existsSync(`${candidate}${sep}${name}`)) {
             return candidate;
         }
-        candidate = candidate.slice(0, candidate.lastIndexOf(sep))
+        candidate = candidate.slice(0, candidate.lastIndexOf(sep));
     }
     return null;
 }
