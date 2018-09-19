70645118eea802cb09c6dd3d09a2ad1ff533cc53
Uriah Ahrak <uriah.achrak@gmail.com>
2018-09-20 01:46:40 +0300

    Added webpack configuration for bundling cli

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+var nodeExternals = require("webpack-node-externals");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const path = require("path");
+const WebpackBar = require("webpackbar");
+// const DeclarationFilesPlugin = require("./declaration");
+
+// Clean configurations
+const clean_paths = ["compiled"];
+
+const clean_options = {
+    watch: true
+};
+
+module.exports = {
+    entry: "./src/cli.ts",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    externals: [nodeExternals()],
+    optimization: {
+        minimize: false
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "compiled"),
+        libraryTarget: "commonjs",
+        library: "bundle"
+    },
+    target: "node",
+    mode: "production",
+    plugins: [
+        new CleanWebpackPlugin(clean_paths, clean_options),
+        new UglifyJsPlugin(),
+        new WebpackBar({
+            name: "Witty"
+        })
+        // new DeclarationFilesPlugin({
+        //     merge: true,
+        //     include: ["CommandAbstract", "IOption"]
+        // })
+    ]
+};
